eb797714130ffcfb8f6fd3ebe190a054fe2a4eca
lukasz.kalinowski.terg@gmail.com <lukasz.sklad@gmail.com>
2025-02-05 19:47:25 +0100

    img vercel import

--- App.jsx
+++ App.jsx
@@ -3,10 +3,13 @@ import VideoPlayer from './src/components/VideoPlayer'
 import DownloadButton from './src/components/DownloadButton'
 import ThemeSwitcher from './src/components/ThemeSwitcher'
 import './src/styles.css'
+import poster1 from './src/img/1.webp'
+import poster2 from './src/img/2.webp'
+import poster3 from './src/img/3.webp'
 
 const App = () => {
     const defaultUrl = 'https://filedn.eu/lPq6O1K7j8DR1n7JwTuYjYz/7ooOOKjskks39jdhhdooommcooodkkywrrqbnx.mp4'
-    const posterImages = ['./src/img/1.webp', './src/img/2.webp', './src/img/3.webp']
+    const posterImages = [poster1, poster2, poster3]
     const getRandomPoster = () => posterImages[Math.floor(Math.random() * posterImages.length)]
     const [theme, setTheme] = useState('dark')
     const [videoUrl, setVideoUrl] = useState('')
