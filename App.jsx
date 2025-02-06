51118d083a44a179f54b64836f811e9fc7fe5f51
lukasz.kalinowski.terg@gmail.com <lukasz.sklad@gmail.com>
2025-02-06 11:25:44 +0100

    poster6

--- App.jsx
+++ App.jsx
@@ -8,10 +8,11 @@ import poster2 from './src/img/2.webp'
 import poster3 from './src/img/3.webp'
 import poster4 from './src/img/4.webp'
 import poster5 from './src/img/5.webp'
+import poster5 from './src/img/6.webp'
 
 const App = () => {
     const defaultUrl = 'https://filedn.eu/lPq6O1K7j8DR1n7JwTuYjYz/7ooOOKjskks39jdhhdooommcooodkkywrrqbnx.mp4'
-    const posterImages = [poster1, poster2, poster3, poster4, poster5]
+    const posterImages = [poster1, poster2, poster3, poster4, poster5, poster6]
     const getRandomPoster = () => posterImages[Math.floor(Math.random() * posterImages.length)]
     const [theme, setTheme] = useState('dark')
     const [videoUrl, setVideoUrl] = useState('')
